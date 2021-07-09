4c00b2571ff2afb4aab2bfacd75e88b5476ebf04
Ali MOUSAZADEH <ali.moosazadeh92@gmail.com>
2021-07-09 21:04:18 +0430

    manifest file

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and salam to reload.
-        </p>
+        </p>4
         <a
           className="App-link"
           href="https://reactjs.org"
