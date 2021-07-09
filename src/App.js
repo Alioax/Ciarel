c8dc1146fc5c3b2e6df8d4cfb6719943208cdeaa
Ali MOUSAZADEH <ali.moosazadeh92@gmail.com>
2021-07-10 00:05:17 +0430

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and salam to reload.
+          Edit <code>src/App.js</code> and salam Ali TAMIMI to reload.
         </p>4
         <a
           className="App-link"
